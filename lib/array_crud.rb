d2dfcb4d74ff4022e26ddd99cfd0f7fd5b6fd0fc
Cara Zellner <carajeanine@gmail.com>
2020-05-30 22:08:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   cats = ["a", "b", "c", "d"]
-  cats.unshift "arrays!"
+  cats.unshift("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
